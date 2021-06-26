27c8dd29bb69de39037e6d5bc888e2e906aa4af2
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-06-26 00:26:57 -0300

    Flight API Methods

---
+++ Components/Flight.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var cors = require('cors');
+
+var mysql = require ('mysql');
+var app = express();
+
+app.use(cors());
+app.use(express.json());
+
+var connection = mysql.createConnection({
+    host: 'localhost', // O host do banco. Ex: localhost
+    user: 'root', // Um usuário do banco. Ex: user 
+    port: 3306,
+    password: 'password', // A senha do usuário. Ex: user123
+    database: 'compania_aereateste1' // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+});
+
+connection.connect(function(error){
+    if(!!error){
+        console.log(error);
+    }else{
+        console.log('Your DB is connected!')
+    }
+});
+
+//Métodos para integração Vôo
+
+app.post('/flight-add', function(req,res){
+    //Criação Das Variáveis:
+    const {Numero_voo, Companhia_aerea, Dias_da_semana} = req.body;
+
+    connection.query(`INSERT INTO voo (Numero_voo, Companhia_aerea, Dias_da_semana) VALUES ('${Numero_voo}', '${Companhia_aerea}', '${Dias_da_semana}')`, function(error, rows){
+        if(!!error){
+            res.send(error.sqlMessage);
+        }else{
+            res.send(rows);
+        }
+    });
+
+});
+
+
+app.get('/flight-list', function(req,res){
+
+    connection.query("SELECT * FROM voo", function(error, rows){
+        if(!!error){
+            res.send(error.sqlMessage);
+        } else {
+            res.send(rows);
+        }
+    });
+
+});
+
+app.put('/flight-update', function(req,res){
+    const {Numero_voo, Companhia_aerea, Dias_da_semana} = req.body;
+
+    connection.query(`UPDATE aeroporto SET Companhia_aerea = '${Companhia_aerea}', Dias_da_semana = '${Dias_da_semana}' WHERE Numero_voo = '${Numero_voo}'`, function(error, rows){
+        if(!!error){
+            res.send(error.sqlMessage);
+        } else {
+            res.send(rows);
+        }
+    });
+
+});
+
+app.delete('/flight-delete', function(req,res){
+    const {Numero_voo} = req.body
+    connection.query(`DELETE FROM aeroporto WHERE Numero_voo = ${Numero_voo}`, function(error, rows){
+        if(!!error){
+            res.send(error.sqlMessage);
+        } else {
+            res.send(rows);
+        }
+    });
+
+});
+
+app.listen(1337);
