5d52f1a491bc4e538852e4ca7f0f1737ec9a551a
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-06-29 22:42:04 -0300

    Adicionando Rate

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ app.use(require('./Components/Airplane.js'));
 app.use(require('./Components/Airport.js'));
 app.use(require('./Components/FlightExcerpt.js'));
 app.use(require('./Components/Flight.js'));
+app.use(require('./Components/Rate.js'));
+
 
 app.listen(1337);
