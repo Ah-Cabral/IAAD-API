403055442eb510b85494a8dfd3498fdbaf2dd778
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-03 08:06:20 -0300

    Updating routes structure

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ app = express()
 
 app.use(require('./Components/Airplane.js'));
 app.use(require('./Components/Airport.js'));
-app.use(require('./Components/FlightExcerpt.js'));
 app.use(require('./Components/Flight.js'));
 app.use(require('./Components/Rate.js'));
-app.use(require('./Components/SnippetInstance.js'));
-app.use(require('./Components/AllowLand.js'));
-app.use(require('./Components/SeatReserve.js'));
 
 app.listen(8080);
