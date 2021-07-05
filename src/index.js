283d5c3bddb08c66aa353f5d24287fa489059a66
Arthur Cabral <arthurcabral.dev@gmail.com>
2021-07-04 23:29:54 -0300

    Inserting New Routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 app = express()
 
-app.use(require('./Components/Airplane.js'));
 app.use(require('./Components/Airport.js'));
-app.use(require('./Components/Flight.js'));
 app.use(require('./Components/Rate.js'));
+app.use(require('./Components/avgRate.js'));
+app.use(require('./Components/FlightCount.js'));
 
 app.listen(3306);
